a81f420893dddf41de49df26599b9f5edf773a09
Kevin Miller <kmille1014@gmail.com>
2018-11-11 12:11:23 -0500

    Fixed realm bugs, needed to include annotation processors for kotlin in gradle and cant use abstract objects with realm

--- app/src/main/java/me/kmmiller/theduckypodcast/core/Extensions.kt
+++ app/src/main/java/me/kmmiller/theduckypodcast/core/Extensions.kt
@@ -1,9 +1,10 @@
 package me.kmmiller.theduckypodcast.core
 
 import io.realm.Realm
-import me.kmmiller.theduckypodcast.base.BaseModel
+import me.kmmiller.theduckypodcast.models.UserModel
 
-
-fun Realm.findById(id: String): BaseModel? {
-    return where(BaseModel::class.java).equalTo("id", id).findFirst()
+/*
+fun Realm.findById(id: String): UserModel? {
+    return where(UserModel::class.java).equalTo("id", id).findFirst()
 }
+*/
--- app/src/main/java/me/kmmiller/theduckypodcast/models/UserModel.kt
+++ app/src/main/java/me/kmmiller/theduckypodcast/models/UserModel.kt
@@ -1,24 +1,29 @@
 package me.kmmiller.theduckypodcast.models
 
 import com.google.firebase.firestore.DocumentSnapshot
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 import io.realm.annotations.RealmClass
-import me.kmmiller.theduckypodcast.base.BaseModel
+import io.realm.annotations.Required
 
 @RealmClass
-class UserModel : BaseModel() {
+open class UserModel : RealmObject() {
+    @Required
+    @PrimaryKey
+    var id: String = ""
 
     var age = 0
     var gender = ""
     var state = ""
 
-    override fun toRealmModel(document: DocumentSnapshot) {
+    fun toRealmModel(document: DocumentSnapshot) {
         id = document.id
         age = document["age"] as? Int ?: 0
         gender = document["gender"] as? String ?: ""
         state = document["state"] as? String ?: ""
     }
 
-    override fun fromRealmModel(): HashMap<String, Any> {
+    fun fromRealmModel(): HashMap<String, Any> {
         val map = HashMap<String, Any>()
         map["age"] = age
         map["gender"] = gender
