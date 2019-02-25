9869708bd9375e5009762fc5a984d77166d75565
Kevin Miller <kmille1014@gmail.com>
2019-02-24 21:21:36 -0500

    Added colorblind mode label to charts frags, and clicking on it jumps to settings

--- app/src/main/java/me/kmmiller/theduckypodcast/main/DailiesResultsFragment.kt
+++ app/src/main/java/me/kmmiller/theduckypodcast/main/DailiesResultsFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import me.kmmiller.theduckypodcast.R
 import me.kmmiller.theduckypodcast.base.BaseResultsFragment
 import me.kmmiller.theduckypodcast.databinding.DailiesResultsFragmentBinding
 import me.kmmiller.theduckypodcast.main.interfaces.NavItem
+import me.kmmiller.theduckypodcast.main.optionsmenu.SettingsFragment
 import me.kmmiller.theduckypodcast.models.ResultsAnswers
+import me.kmmiller.theduckypodcast.utils.ColorblindPreferences
 
 class DailiesResultsFragment : BaseResultsFragment(), NavItem {
     private lateinit var binding: DailiesResultsFragmentBinding
@@ -37,11 +40,30 @@ class DailiesResultsFragment : BaseResultsFragment(), NavItem {
             binding.swipeRefreshLayout.isRefreshing = true
             onRefresh()
         }
+
+        binding.cbModeLabel.text = when(ColorblindPreferences.getCbMode(requireActivity())) {
+            ColorblindPreferences.ColorblindMode.OFF -> String.format(getString(R.string.cb_mode_label), getString(R.string.off))
+            ColorblindPreferences.ColorblindMode.DEUTERANOMALY -> String.format(getString(R.string.cb_mode_label), getString(R.string.deuteranomaly))
+            ColorblindPreferences.ColorblindMode.PROTANOMALY -> String.format(getString(R.string.cb_mode_label), getString(R.string.protanomaly))
+            ColorblindPreferences.ColorblindMode.TRITANOMOLY -> String.format(getString(R.string.cb_mode_label), getString(R.string.tritanomoly))
+        }
+
+        binding.eyeMode.setImageDrawable(when(ColorblindPreferences.getCbMode(requireActivity())) {
+            ColorblindPreferences.ColorblindMode.OFF -> ResourcesCompat.getDrawable(resources, R.drawable.ic_cb_eye_off, null)
+            ColorblindPreferences.ColorblindMode.DEUTERANOMALY -> ResourcesCompat.getDrawable(resources, R.drawable.ic_cb_eye_d, null)
+            ColorblindPreferences.ColorblindMode.PROTANOMALY -> ResourcesCompat.getDrawable(resources, R.drawable.ic_cb_eye_p, null)
+            ColorblindPreferences.ColorblindMode.TRITANOMOLY -> ResourcesCompat.getDrawable(resources, R.drawable.ic_cb_eye_t, null)
+        })
+
+        binding.cbModeLabel.setOnClickListener { onCbModeClicked() }
+        binding.eyeMode.setOnClickListener { onCbModeClicked() }
     }
 
     private fun onRefresh() {
         viewModel?.dailyId?.let { dailyId ->
+            showProgress(getString(R.string.loading))
             getCollectedResponses("dailies-responses", dailyId) {
+                dismissProgress()
                 drawAllCharts(it)
             }
         }
@@ -51,6 +73,10 @@ class DailiesResultsFragment : BaseResultsFragment(), NavItem {
         binding.swipeRefreshLayout.isRefreshing = false
         binding.chartList.adapter = getChartsAdapter(resultsAnswers)
         binding.chartList.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun onCbModeClicked() {
+        pushFragmentSynchronous(SettingsFragment(), true, SettingsFragment.TAG)
     }
 
     private fun isNewSubmission(): Boolean = arguments?.getBoolean(NEW_SUBMISSION, false) ?: false
--- app/src/main/java/me/kmmiller/theduckypodcast/main/WeekliesResultsFragment.kt
+++ app/src/main/java/me/kmmiller/theduckypodcast/main/WeekliesResultsFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import me.kmmiller.theduckypodcast.R
 import me.kmmiller.theduckypodcast.base.BaseResultsFragment
 import me.kmmiller.theduckypodcast.databinding.WeekliesResultsFragmentBinding
 import me.kmmiller.theduckypodcast.main.interfaces.NavItem
+import me.kmmiller.theduckypodcast.main.optionsmenu.SettingsFragment
 import me.kmmiller.theduckypodcast.models.ResultsAnswers
+import me.kmmiller.theduckypodcast.utils.ColorblindPreferences
 
 class WeekliesResultsFragment : BaseResultsFragment(), NavItem {
     private lateinit var binding: WeekliesResultsFragmentBinding
@@ -37,11 +40,30 @@ class WeekliesResultsFragment : BaseResultsFragment(), NavItem {
             binding.swipeRefreshLayout.isRefreshing = true
             onRefresh()
         }
+
+        binding.cbModeLabel.text = when(ColorblindPreferences.getCbMode(requireActivity())) {
+            ColorblindPreferences.ColorblindMode.OFF -> String.format(getString(R.string.cb_mode_label), getString(R.string.off))
+            ColorblindPreferences.ColorblindMode.DEUTERANOMALY -> String.format(getString(R.string.cb_mode_label), getString(R.string.deuteranomaly))
+            ColorblindPreferences.ColorblindMode.PROTANOMALY -> String.format(getString(R.string.cb_mode_label), getString(R.string.protanomaly))
+            ColorblindPreferences.ColorblindMode.TRITANOMOLY -> String.format(getString(R.string.cb_mode_label), getString(R.string.tritanomoly))
+        }
+
+        binding.eyeMode.setImageDrawable(when(ColorblindPreferences.getCbMode(requireActivity())) {
+            ColorblindPreferences.ColorblindMode.OFF -> ResourcesCompat.getDrawable(resources, R.drawable.ic_cb_eye_off, null)
+            ColorblindPreferences.ColorblindMode.DEUTERANOMALY -> ResourcesCompat.getDrawable(resources, R.drawable.ic_cb_eye_d, null)
+            ColorblindPreferences.ColorblindMode.PROTANOMALY -> ResourcesCompat.getDrawable(resources, R.drawable.ic_cb_eye_p, null)
+            ColorblindPreferences.ColorblindMode.TRITANOMOLY -> ResourcesCompat.getDrawable(resources, R.drawable.ic_cb_eye_t, null)
+        })
+
+        binding.cbModeLabel.setOnClickListener { onCbModeClicked() }
+        binding.eyeMode.setOnClickListener { onCbModeClicked() }
     }
 
     private fun onRefresh() {
         viewModel?.weeklyId?.let { weeklyId ->
+            showProgress(getString(R.string.loading))
             getCollectedResponses("weeklies-responses", weeklyId) {
+                dismissProgress()
                 drawAllCharts(it)
             }
         }
@@ -51,6 +73,10 @@ class WeekliesResultsFragment : BaseResultsFragment(), NavItem {
         binding.swipeRefreshLayout.isRefreshing = false
         binding.chartList.adapter = getChartsAdapter(resultsAnswers)
         binding.chartList.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun onCbModeClicked() {
+        pushFragmentSynchronous(SettingsFragment(), true, SettingsFragment.TAG)
     }
 
     private fun isNewSubmission(): Boolean = arguments?.getBoolean(NEW_SUBMISSION, false) ?: false
